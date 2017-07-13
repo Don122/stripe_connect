a5d384fe7dc439b9673baa657ad946bd2d695e19
laurie@friendlyroad.com <laurie@friendlyroad.com>
2017-07-13 17:35:50 -0600

    fix stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,5 @@
 require 'stripe'
 
-# Stripe.api_key = Rails.application.secrets.stripe_secret_key
-# Srtipe.api_key = sk_test_kHUYaU1gm5jOkU1rRgCTZDj4 
-Stripe.api_key = 'sk_test_kHUYaU1gm5jOkU1rRgCTZDj4'
+Stripe.api_key = Rails.application.secrets.stripe_secret_key
+
 Stripe.api_version = '2017-04-06'
